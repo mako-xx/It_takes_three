9e28c24c8ddebd18816572dc21a2e11f41ccb397
Faust <1546327522@qq.com>
2021-12-21 14:33:44 +0800

    周边搜索初步实现

---
+++ app/src/main/java/com/example/ittakesthree/map/poisearch/PoiSearchAroundActivity.java
@@ -0,0 +1,14 @@
+package com.example.ittakesthree.map.poisearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PoiSearchAroundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poi_search_around);
+    }
+}
