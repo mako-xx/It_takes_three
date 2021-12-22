50599c95b9247e6aca6dcb6f55db8070ebd3d7c2
Faust <1546327522@qq.com>
2021-12-22 16:18:41 +0800

    景点搜索，导航详细数据，定位

---
+++ app/src/main/java/com/example/ittakesthree/pojo/PicList.java
@@ -0,0 +1,30 @@
+/**
+  * Copyright 2021 bejson.com 
+  */
+package com.example.ittakesthree.pojo;
+
+/**
+ * Auto-generated: 2021-12-22 15:30:57
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class PicList {
+
+    private String picUrl;
+    private String picUrlSmall;
+    public void setPicUrl(String picUrl) {
+         this.picUrl = picUrl;
+     }
+    public String getPicUrl() {
+         return picUrl;
+     }
+
+    public void setPicUrlSmall(String picUrlSmall) {
+         this.picUrlSmall = picUrlSmall;
+     }
+    public String getPicUrlSmall() {
+         return picUrlSmall;
+     }
+
+}
